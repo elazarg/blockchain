d084f14a6593e31a172735f3ffb0b55ee4492d83
Elazar Gershuni <elazarg@gmail.com>
2016-11-28 21:27:49 +0200

    modularize

--- MachineModel.v
+++ MachineModel.v
@@ -4,13 +4,9 @@ Import ListNotations.
 Require Import Arith.PeanoNat.
 Require Import Relations.Relation_Definitions.
 
-Require Import
-  FSets.FMapWeakList
-  FSets.FSetWeakList
-  Structures.DecidableType.
+Require Import FSets.FSetWeakList.
 
 Module Import Collection := FSetWeakList.Make Nat.
-
 Definition TransactionRequest := nat.
 
 Parameter PersistentState : Type.
@@ -34,9 +30,3 @@ Definition add_request (m : Machine) (request : TransactionRequest) : Machine :=
   in Build_Machine (add request board) history now.
 
 Definition empty_machine := Build_Machine empty [] startState.
-
-Inductive machine_step (m m': Machine) : Prop :=
-  | machine_tx_perform : forall request, mem request (billboard m') = false
-                        -> m' = perform_request m request
-                        -> machine_step m m'.
-
--- firstModel.v
+++ firstModel.v
@@ -1,60 +1,24 @@
-Require Import Bool.
-Require Import List.
-Import ListNotations.
+Require Import MachineModel.
 Require Import Arith.PeanoNat.
-Require Import Relations.Relation_Definitions.
 
 Require Import
   FSets.FMapWeakList
   FSets.FSetWeakList
   Structures.DecidableType.
 
-Module Import Collection := FSetWeakList.Make Nat.
-
-Definition TransactionRequest := nat.
-
-Parameter PersistentState : Type.
-
-Parameter startState : PersistentState.
-Parameter law : PersistentState -> TransactionRequest -> PersistentState.
-
-
-Record Machine : Type := {
-  billboard : Collection.t;
-  history : list PersistentState;
-  now : PersistentState;
-}.
-
-Definition perform_request (m : Machine) (request : TransactionRequest) : Machine :=
-  let (board, history, now) := m
-  in Build_Machine (remove request board) (now::history) (law now request).
-
-Definition add_request (m : Machine) (request : TransactionRequest) : Machine :=
-  let (board, history, now) := m
-  in Build_Machine (add request board) history now.
-
-Definition empty_machine := Build_Machine empty [] startState.
-
 Module Import Map := FMapWeakList.Make Nat.
 Definition Identity := nat.
 
 Definition Strategy := Machine -> TransactionRequest.
 
-Record Game : Type := {
-  agents : Map.t Strategy;
-  machine : Machine;
-}.
-
-Inductive game_step (agents : Map.t Strategy) (g g' : Game) : Prop :=
-  | move_machine : forall m m' request,
-                   m <> m' /\ m' = perform_request m request
-                   -> g = (Build_Game agents m) /\ g' = (Build_Game agents m')
-                   -> game_step agents g g'
+Inductive arena_step (agents : Map.t Strategy) (m m' : Machine) : Prop :=
+  | move_machine : forall request, m <> m' /\ m' = perform_request m request
+                   -> arena_step agents m m'
   | move_agents : forall request agent,
-                  Some request = find agent (map (fun s => s (machine g)) agents)
-                  -> Collection.mem request (billboard (machine g)) = false
-                  -> g = (Build_Game agents (machine g)) /\ g' = (Build_Game agents (add_request (machine g) request))
-                  -> game_step agents g g'.
+                  Some request = find agent (map (fun s => s m) agents)
+                  -> Collection.mem request (billboard m) = false
+                  -> m' = add_request m request
+                  -> arena_step agents m m'.
 
 (*
 Decisions:
