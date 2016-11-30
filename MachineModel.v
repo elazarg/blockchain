ebb99a9442b84c032d0c4fdc8fb95d87551b242a
Elazar Gershuni <elazarg@gmail.com>
2016-11-30 14:41:50 +0200

    step-consistent deal

--- MachineModel.v
+++ MachineModel.v
@@ -11,7 +11,30 @@ Definition SpecId := nat.
 Definition SpecMap := MapNat.t.
 
 Variable LocalPState : Type.
-Definition GlobalPState := SpecId -> LocalPState.
+Definition GlobalPState := SpecMap LocalPState.
+
+(* TODO: perhaps not a SpecMap but a RoleMap *)
+(* A deal is a 'lockstep' of group of businesses *)
+Definition Deal := SpecMap (LocalPState * LocalPState).
+(* Policies must be encoded in the local state, so other policies can depend on them *)
+Definition PolicyExtractor := LocalPState -> (Deal -> Prop).
+
+Variable  (allows : PolicyExtractor).
+
+(* Perhaps we may need to approve only the resulting state *)
+Inductive legal_deal (me : SpecId) (deal : Deal) : Prop :=
+  | base : forall start finish,
+           find me deal = Some (start, finish)
+        -> allows start deal
+        -> legal_deal me deal
+  | step : forall partial_deal,
+           legal_deal me partial_deal
+        -> partial_deal = remove me deal
+        -> legal_deal me deal.
+
+Definition step_consistent_deal (deal : Deal) : Prop :=
+  forall (spec_id : SpecId), legal_deal spec_id deal.
+
 Definition Request : Type := LocalPState * LocalPState.
 (* spec_of owner -> someother -> transition *)
 
@@ -26,13 +49,6 @@ Variable agents : AgentMap (Machine -> Request).
 
 Parameter spec_of : AgentId -> SpecId.
 Variable startState : GlobalPState.
-
-(* TODO: perhaps not a SpecMap but a RoleMap *)
-Definition Deal := SpecMap (LocalPState * LocalPState).
-(* Policies must be encoded in the local state, so other policies can depend on them *)
-Definition SingleBusinessPolicy := LocalPState -> (Deal -> Prop).
-
-Variable lawbook : SpecMap (SpecId -> SingleBusinessPolicy).
 
 Definition add_request (owner : AgentId) (request : Request) (m : Machine) : Machine :=
   Build_Machine (add owner request (billboard m)) (history m) (now m).
