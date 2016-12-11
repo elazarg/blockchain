24bc337cae07e693ebaf4f75a29eaef9bdaf7017
Elazar Gershuni <elazarg@gmail.com>
2016-12-11 23:53:04 +0200

    Before language

--- MachineModelTriplet.v
+++ MachineModelTriplet.v
@@ -14,50 +14,29 @@ Definition BusinessMap := MapNat.t.
 Definition GlobalPState := BusinessMap LocalPState.
 
 Section Deal.
-(* A deal is a 'lockstep' of group of businesses *)
-(* TODO: perhaps not a BusinessMap but a RoleMap *)
-Definition Deal := BusinessMap (LocalPState * LocalPState).
-(* Policies must be encoded in the local state, so other policies can depend on them *)
-Definition PolicyExtractor := LocalPState -> (Deal -> Prop).
 
-Definition unzip_deal (deal : Deal) := (map fst deal, map snd deal).
+Definition Deal := (BusinessId * BusinessId * BusinessId) % type.
+Parameter allows : BusinessId -> (BusinessId * BusinessId) -> option LocalPState.
 
-Parameter (allows : PolicyExtractor).
-
-(* Perhaps we may need to approve only the resulting state *)
-(* The inductive definition is only here to say "legal deals are superset-closed" *)
 Definition step_legal_deal (me : BusinessId) (deal : Deal) : Prop :=
-  exists partial_deal,
-       (forall k v, MapsTo k v partial_deal -> MapsTo k v deal)
-    /\ (exists start finish, MapsTo me (start, finish) partial_deal /\ allows start partial_deal).
+  let '(a, b, c) := deal in
+     (me = a /\ (allows me (b, c) <> None \/ allows me (c, b) <> None))
+  \/ (me = b /\ (allows me (a, c) <> None \/ allows me (c, a) <> None))
+  \/ (me = c /\ (allows me (a, b) <> None \/ allows me (b, a) <> None)).
 
-Definition composed_deals (d1 d2 res : Deal) : Prop :=
-  let '(a, b1) := unzip_deal d1 in
-  let '(b2, c) := unzip_deal d2 in
-  forall id a b, MapsTo id b1 a -> MapsTo id b2 b -> a = b.
-
-
-(* A deal is consistent if it can be seen as a sequence of deals such that
-   each deal is step-consistent. (sadly, the second constructor is not uniquely defined) *)
-Inductive consistent_deal (deal : Deal) : Prop :=
-  | consistent_base : forall (b : BusinessId), step_legal_deal b deal
-                        -> consistent_deal deal
-  | consistent_compose (d1 d2 : Deal) : composed_deals d1 d2 deal
-                        -> consistent_deal deal.
+Definition consistent_deal (deal : Deal) : Prop :=
+  let '(a, b, c) := deal in
+  step_legal_deal a deal /\ step_legal_deal b deal /\ step_legal_deal c deal.
 
 
 End Deal.
 
 Record Machine : Type := {
-  history : list GlobalPState;
-  now : GlobalPState;
+  history : list Deal;
 }.
 
 Inductive machine_step : Machine -> Machine -> Prop :=
-  | move_machine (m1 m2: Machine) :
-                   history m2 = (now m1)::(history m1)
-                -> (exists deal, unzip_deal deal = (now m1, now m2) /\ consistent_deal deal)
-                -> machine_step m1 m2.
+  .
 
 Variable startState : GlobalPState.
 End MachineDef.
