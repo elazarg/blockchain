0ce9e267469c084d69d15e18527a3353c364a710
Elazar Gershuni <elazarg@gmail.com>
2016-12-22 18:44:49 +0200

    memory

--- Memory.v
+++ Memory.v
@@ -7,8 +7,10 @@ Require Import Maps.
 Require Import List.
 Import ListNotations.
 
-Definition memory := ZMap.t byte.
-Definition mget (addr : word) (m : memory) : byte := (ZMap.get (Word.intval addr) m).
+Module WordMap := ZMap.
+
+Definition memory := WordMap.t byte.
+Definition mget (addr : word) (m : memory) : byte := (WordMap.get (Word.intval addr) m).
 
 Definition mem_store_byte (addr : word) (b : byte) (m : memory) : memory :=
   (ZMap.set (Word.intval addr) b m).
@@ -17,7 +19,7 @@ Definition mem_store_first_byte (addr : word) (w : word) (m : memory) : memory :
   (ZMap.set (Word.intval addr) (Byte.repr (Z.modulo (Word.intval w) 256)) m).
 
 Definition mem_read_word (addr : word) (m : memory) : word :=
-  let b := fun n => ZMap.get (Z.add (Word.intval addr) n) m in
+  let b := fun n => WordMap.get (Z.add (Word.intval addr) n) m in
   word_from_bytes (b 0%Z) (b 1%Z) (b 2%Z) (b 3%Z) (b 4%Z) (b 5%Z) (b 6%Z) (b 7%Z).
 
 Definition mem_store_word (w : word) (addr : word) (m : memory) : memory :=
@@ -33,21 +35,17 @@ Definition exec_mem_instr (i : mem_instruction) (m : memory) (ws : list word) : 
     | _, _ => None
   end.
 
-Definition storage := MapNat.t word.
+Definition storage := WordMap.t word.
 
-Definition storage_read (p: storage) (addr: word) : word :=
-  match find addr p with
-    | Some v => v
-    | None => 0
-  end.
-Definition storage_store (p: storage) (addr: word) (w: word) : memory :=
-  add addr w p.
+Definition storage_read (addr: word) (p: storage) : word := WordMap.get (Word.intval addr) p.
+
+Definition storage_store (addr: word) (w: word) (p: storage) : storage := WordMap.set (Word.intval addr) w p.
 
 (* storage should be indexed by contract id *)
 Definition exec_storage_instr (i : storage_instruction) (p : storage) (ws : list word) : option (storage * list word) :=
   match i, ws with
-    | I_SLOAD, addr::ws => Some (p, storage_read p addr :: ws)
-    | I_SSTORE, addr::val::ws => Some (storage_store p addr val, ws)
+    | I_SLOAD, addr::ws => Some (p, storage_read addr p :: ws)
+    | I_SSTORE, addr::val::ws => Some (storage_store addr val p, ws)
     | _, _ => None
   end.
 
